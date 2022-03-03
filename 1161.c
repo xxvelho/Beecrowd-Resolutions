d99386daede8dd8fd1a55832fbdc90f0831e7335
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-03 18:28:50 -0300

    Create 1161.c

---
+++ 1161.c
@@ -0,0 +1,25 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1161 - Soma de Fatoriais;
+*/
+
+#include <stdio.h>
+
+//função recussiva fibonacci
+long long int fat(int n) {
+  if (n==0) return 1;
+  else return n * fat(n-1);
+}
+
+int main () {
+  long long int SUM;
+  int M, N;
+  
+  while(scanf("%d %d", &M, &N) != EOF){
+
+    SUM = (fat(M) + fat(N));
+    printf("%lld\n", SUM);
+    
+  }
+  return 0;
+}
