d796d6825111ca8ad271f921d628642a8d856432
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-03 17:52:09 -0300

    Create 1095.c

---
+++ 1095.c
@@ -0,0 +1,15 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1095 - Sequencia IJ 1;
+*/
+
+#include <stdio.h>
+
+int main() {
+  int i,j;
+  for(i=1,j=60; i<=100 && j>=0; i+=3,j-=5){
+    printf("I=%d J=%d\n", i, j);
+  }
+
+return 0;
+}
