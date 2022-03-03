f2769bc36cb486d8911f18c8b85f989049422635
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-03 18:32:39 -0300

    Create 1864.c

---
+++ 1864.c
@@ -0,0 +1,19 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1864 - Nossos Dias Nunca Voltarão;
+*/
+
+#include <stdio.h>
+
+int main(){
+    int n,i;
+    char v[40]="LIFE IS NOT A PROBLEM TO BE SOLVED";
+    
+    scanf("%d", &n);
+    
+    for(i=0; i<n; i++)
+        printf("%c", v[i]);
+    printf("\n");
+  
+    return 0;
+}
