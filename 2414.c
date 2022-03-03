5afef37a81b95fe6a8fcea97d3e481443ea424a8
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-03 18:34:01 -0300

    Create 2414.c

---
+++ 2414.c
@@ -0,0 +1,21 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 2414 - Desafio do Maior Número;
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+  int n, maior = 0;
+    
+  while (scanf("%d", &n) != EOF){
+    if (n > maior){
+      maior = n;
+    }
+  }
+    
+  printf("%d\n", maior);
+
+  return 0;
+}
