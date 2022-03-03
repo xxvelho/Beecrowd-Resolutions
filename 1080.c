aa1ac871e26c7ec5c1e7e9c76817cf86c900e16e
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-03 17:51:24 -0300

    Create 1080.c

---
+++ 1080.c
@@ -0,0 +1,23 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1080 - Maior e Posição;
+*/
+
+#include <stdio.h>
+
+int main () {
+  
+  int i, num, maior = 0, posicao;
+
+  for(i=1; i<=100;i++){
+    scanf("%d", &num);
+    if(maior < num){
+      maior = num;
+      posicao = i;
+    }
+  }
+
+  printf("%d\n%d\n", maior, posicao);
+
+  return 0;
+}
