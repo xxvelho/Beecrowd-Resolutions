fef80db75585ecf0cf5d44812909dbaac26dc234
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-03 17:50:02 -0300

    Create 1074.c

---
+++ 1074.c
@@ -0,0 +1,38 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1074 - Par ou Ímpar;
+*/
+
+#include <stdio.h>
+
+int main(){
+
+int n, num, i;
+scanf("%d", &n);
+
+for(i=0;i<n;i++){
+  
+  scanf("%d", &num);
+  
+  if(num ==0){
+    printf("NULL\n");
+  }
+  
+  else if (num%2==0 && num > 0){
+    printf("EVEN POSITIVE\n");
+  }
+  
+  else if (num%2==0 && num < 0){
+    printf("EVEN NEGATIVE\n");
+  }
+  
+  else if (num%2!=0 && num > 0){
+    printf("ODD POSITIVE\n");
+  }
+  
+  else if (num%2!=0 && num < 0){
+    printf("ODD NEGATIVE\n");
+  }
+}
+
+return 0;
