0704870011d8b014799053b94e9a2e9df959a1f9
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-03 18:23:43 -0300

    Create 1099.c

---
+++ 1099.c
@@ -0,0 +1,32 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1099 - Soma de Ímpares Consecutivos II;
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+  int n, i;
+  scanf("%d", &n);
+
+  for(i = 0; i<n; i++){
+    int i, a,b,aux,soma = 0;
+    scanf("%d %d", &a, &b);
+    if(a>b){
+      aux = a;
+      a = b;
+      b = aux;
+    }
+    for(i=a+1; i<b; i++){
+      if(i%2!=0){
+        soma += i;
+        
+      }
+    }
+
+    printf("%d\n", soma);
+  }
+
+  return 0;
+}
