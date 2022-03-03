eca051f2694df70aab4da13726860a29581cff99
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-03 17:50:42 -0300

    Create 1078.c

---
+++ 1078.c
@@ -0,0 +1,19 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1078 - Tabuada;
+*/
+
+#include <stdio.h>
+
+int main() {
+  int i, num;
+  scanf("%d", &num);
+
+  if(num>2 && num<1000){
+    for(i=1;i<11;i++){
+      printf("%d x %d = %d\n",i, num, num*i);
+    }
+  }
+
+return 0;
+}
