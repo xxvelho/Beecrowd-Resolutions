8b8e3667c9b90dc9ceb4d4c64bc2f08966634aee
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-12 16:25:36 -0300

    Create 3302.c

---
+++ 3302.c
@@ -0,0 +1,19 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 3302 - Resposta Certa;
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  int n, i;
+  scanf("%d", &n);
+  
+  for(i=1;i<=n;i++){
+    int num;
+    scanf("%d", &num);
+    printf("resposta %d: %d\n", i, num);
+  }
+  
+return 0;
+}
