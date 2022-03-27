298c47cb0c3ce78aa3e24485c8f4559c28d9f0cd
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-03-27 20:33:05 -0300

    Create 1020.c

---
+++ 1020.c
@@ -0,0 +1,23 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1020 - Idade em Dias;
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+int dia, mes, ano;
+
+scanf("%d", &dia);
+
+ano = dia / 365;
+dia-=(365*ano);
+
+mes = dia / 30;
+dia-=(30*mes);
+
+printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", ano, mes,dia);
+
+return 0;
+}
