0015fe9399fe065b611b0de00cec2fbeb829ebcd
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-04-01 18:04:20 -0300

    Create 1075.c

---
+++ 1075.c
@@ -0,0 +1,21 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1075 - Resto 2;
+*/
+
+
+#include <stdio.h>
+
+int main (){
+
+  int num, i;
+  scanf("%d", &num);
+
+  for(i = 0; i < 10000; i++){
+    if(i%num==2){
+      printf("%d\n", i);
+    }
+  }  
+
+  return 0;
+}
