d57727077b51bcce40a1a8201bf885825ee422c1
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-04-06 23:03:33 -0300

    Create 1113.c

---
+++ 1113.c
@@ -0,0 +1,27 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1113 - Crescente e Decrescente;
+*/
+
+
+#include <stdio.h>
+
+int main (){
+  int x, y;
+  
+  while(1){
+    scanf("%d %d", &x, &y);
+
+    if(x==y){
+      break;
+    }
+    else if(x<y){
+      printf("Crescente\n");
+    }
+    else {
+      printf("Decrescente\n");
+      }
+    }
+
+  return 0;
+}
