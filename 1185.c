68f0ae0b700cb3dec734a443163e8098ea131572
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-05-27 16:25:33 -0300

    Create 1185.c

---
+++ 1185.c
@@ -0,0 +1,33 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1185 - Acima da Diagonal Secundária;
+*/
+
+#include <stdio.h>
+#define TAM 12
+
+int main(void) {
+  int i, j;
+  char op;
+  double M[TAM][TAM], soma = 0.0, media;
+  scanf("%c", &op);
+  
+  for(i=0; i<TAM; i++){
+    for(j=0; j<TAM; j++){
+      scanf("%lf", &M[i][j]);
+      if(j<TAM-i-1){
+        soma+=M[i][j];
+      }
+    }
+  }
+
+  if(op=='S'){
+    printf("%.1lf\n", soma);
+  }
+  else if(op=='M'){
+    media = soma/66.0;
+    printf("%.1lf\n", media);
+  }
+  
+  return 0;
+}
