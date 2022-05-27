db44ffe8c7b11410d25a8d9fcadb44f65b800e50
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-05-27 15:42:39 -0300

    Create 1188.c

---
+++ 1188.c
@@ -0,0 +1,40 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1188 - Área Inferior ;
+*/
+
+#include <stdio.h>
+#define TAM 12
+
+int main(void) {
+  int i, j;
+  char op;
+  double M[TAM][TAM], soma = 0.0, mult=1.0;
+  scanf("%c", &op);
+  for(i=0; i<TAM; i++)
+  for(j=0;j<TAM; j++)
+    scanf("%lf", &M[i][j]);
+
+  if(op=='S'){
+    for(i=0; i<TAM; i++){
+      for(j=0; j<TAM; j++){
+        if(i>j && j>TAM-i-1){
+          soma+=M[i][j];
+        }
+      }
+    }
+    printf("%.1lf\n", soma);
+  }
+  else if(op=='M'){
+    for(i=0; i<TAM; i++){
+      for(j=0; j<TAM; j++){
+        if(i>j && j>TAM-i-1){
+          mult*=M[i][j];
+        }
+      }
+    }
+    printf("%.1lf\n", mult);
+  }
+  
+  return 0;
+}
