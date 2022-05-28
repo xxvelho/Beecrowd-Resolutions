a6875f4a758de92e7ca65da8bc6b1b982e1dad49
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-05-28 15:09:07 -0300

    Create 2413.c

---
+++ 2413.c
@@ -0,0 +1,15 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 2413 - Busca na Internet;
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int num;
+  
+    scanf("%d", &num);
+    printf("%d\n", num*4);
+
+    return 0;
+}
