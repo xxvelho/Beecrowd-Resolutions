a9425ae43da1a0eca017563aeb00081472a7b0cf
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-05-28 15:35:27 -0300

    Create 2344.c

---
+++ 2344.c
@@ -0,0 +1,24 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 2344 - Notas da Prova;
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int nota;
+    scanf("%d", &nota);
+
+    if(nota>=86 && nota<=100)
+        printf("A\n");
+    else if(nota>=61 && nota<=85)
+        printf("B\n");
+    else if(nota>=36 && nota<=60)
+        printf("C\n"); 
+    else if(nota>=1 && nota<=35)
+        printf("D\n");
+    else
+        printf("E\n"); 
+
+    return 0;
+}
