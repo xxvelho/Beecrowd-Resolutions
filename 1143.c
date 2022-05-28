8990f73262b07bb668c0e6db8804b8e40fa28455
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-05-28 15:29:50 -0300

    Create 1143.c

---
+++ 1143.c
@@ -0,0 +1,17 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 1143 - Quadrado e ao Cubo;
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int num, i;
+    scanf("%d", &num);
+
+    for(i=1; i<=num; i++){
+        printf("%d %d %d\n", i, i*i, i*i*i);
+    }
+
+    return 0;
+}
