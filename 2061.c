8349253e4b8cd3b335775fb4217851011cbe8c41
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-05-28 16:34:30 -0300

    Create 2061.c

---
+++ 2061.c
@@ -0,0 +1,24 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 2061 - As Abas de Péricles;
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int abas, cliques, i;
+    char acao[7];
+    scanf("%d %d", &abas, &cliques);
+
+    for(i=0; i<cliques; i++){
+        scanf("%s", &acao);
+        if(acao[0] == 'f')
+            abas++;
+        else
+            abas--;
+    }
+
+    printf("%d\n", abas);
+
+    return 0;
+}
