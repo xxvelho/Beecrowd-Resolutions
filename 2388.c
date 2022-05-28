76a06ba1a7a6e93df33fce5bd724b7fd00d2412f
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-05-28 15:43:49 -0300

    Create 2388.c

---
+++ 2388.c
@@ -0,0 +1,19 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 2388 - Tacógrafo;
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int n, i, distancia = 0, t,v;
+    scanf("%d", &n);
+
+    for(i=0; i<n; i++){
+        scanf("%d %d", &t, &v);
+        distancia+=(t*v);
+    }
+    printf("%d\n", distancia);
+
+    return 0;
+}
