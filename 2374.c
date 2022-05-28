d3fd482f42f0624a7df044e50f743480a217685a
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-05-28 15:12:51 -0300

    Create 2374.c

---
+++ 2374.c
@@ -0,0 +1,15 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 2374 - Pneu;
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int p_desejada, p_lida;
+    
+    scanf("%d %d", &p_desejada, &p_lida);
+    printf("%d\n", p_desejada-p_lida);
+
+    return 0;
+}
