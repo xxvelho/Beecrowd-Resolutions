af534d72aedf64a2e31f1cc95674f5201222d3be
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-05-28 14:59:46 -0300

    Create 2006.c

---
+++ 2006.c
@@ -0,0 +1,21 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 2006 - Identificando o Chá;
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int participantes[5];
+    int cha, acertos = 0, i;
+
+    scanf("%d", &cha);
+    for (i = 0; i < 5; i++){
+        scanf("%d", &participantes[i]);
+        if(participantes[i]==cha)
+            acertos++;
+    }
+    printf("%d\n", acertos);
+
+    return 0;
+}
