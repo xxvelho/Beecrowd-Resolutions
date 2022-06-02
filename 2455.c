a0c1c454b261400f0e68228b4a31eeca0c058202
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-06-02 15:08:20 -0300

    Create 2455.c

---
+++ 2455.c
@@ -0,0 +1,20 @@
+/*
+  autor: Elivelton Bouteille;
+  nome: 2455 - Gangorra;
+*/
+
+#include <stdio.h>
+
+int main(void){
+  int p1, p2, c1, c2;
+  scanf("%d %d %d %d", &p1, &c1, &p2, &c2);
+  
+  if(p1*c1==p2*c2)
+    printf("0\n");
+  else if(p1*c1>p2*c2)
+    printf("-1\n");
+  else
+    printf("1\n");
+
+  return 0;
+}
