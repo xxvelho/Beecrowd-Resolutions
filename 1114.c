6722683b6133342ea414b254bec33e8a3f132cba
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-06-13 16:47:52 -0300

    Update 1114.c

--- 1114.c
+++ 1114.c
@@ -1,19 +1,16 @@
-/*
-  autor: Elivelton Bouteille;
-  nome: 1114 - Senha Fixa;
-*/
+// 1114 - Senha Fixa;
 
 #include <stdio.h>
 
 int main(){
 int senha;
-  
 scanf("%d", &senha);
 
 while(senha!=2002){
   printf("Senha Invalida\n");
   scanf("%d", &senha);
 }
+  
 printf("Acesso Permitido\n");
 
 return 0;
