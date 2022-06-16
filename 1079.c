ae8a05092cf5f1ae4d8bfd073d62248aa362ba9c
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-06-16 11:07:47 -0300

    Update 1079.c

--- 1079.c
+++ 1079.c
@@ -1,23 +1,15 @@
-/*
-  autor: Elivelton Bouteille;
-  nome: 1079 - Médias Ponderadas;
-*/
-
+// 1079 - Médias Ponderadas;
 
 #include <stdio.h>
 
 int main (){
-
   int n, i;
   double a,b,c, media;
-
   scanf("%d", &n);
 
   for(i=0; i<n;i++){
     scanf("%lf %lf %lf", &a, &b, &c);
-
     media = ((a*2+b*3+c*5)/10);
-
     printf("%.1lf\n", media);
   }
   
