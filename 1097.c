5a0fd6bad0b06b32f887f37446039cb8fa39f279
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-06-19 12:33:23 -0300

    Update 1097.c

--- 1097.c
+++ 1097.c
@@ -1,12 +1,8 @@
-/*
-  autor: Elivelton Bouteille;
-  nome: 1097 - Sequencia IJ 3;
-*/
+// 1097 - Sequencia IJ 3;
 
 #include <stdio.h>
 
 int main(){
-
 int i = 1, j = 7;
   while (i <= 9){
     printf("I=%d J=%d\n", i, j--);
