74a096ca4b40e2fa2006df5b1687e881aa6f5236
Elivelton Bouteille <93676425+xxvelho@users.noreply.github.com>
2022-06-28 10:28:37 -0300

    Update 2374.c

--- 2374.c
+++ 2374.c
@@ -1,13 +1,9 @@
-/*
-  autor: Elivelton Bouteille;
-  nome: 2374 - Pneu;
-*/
+// 2374 - Pneu;
 
 #include <stdio.h>
 
 int main(void){
     int p_desejada, p_lida;
-    
     scanf("%d %d", &p_desejada, &p_lida);
     printf("%d\n", p_desejada-p_lida);
 
